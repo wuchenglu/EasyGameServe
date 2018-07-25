c3c85887f714ec91c2fa962f84dd4e8f84df7065
251020148 <251020148@qq.com>
2018-07-25 22:41:59 +0800

    添加网络核心模块源码

---
+++ MyServer/NetFrame/UserTokenPool.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrame
+{
+    public class UserTokenPool
+    {
+        private Stack<UserToken> Pool;
+
+        public UserTokenPool (int maxNum)
+        {
+            Pool=new Stack<UserToken>(maxNum);
+        }
+        /// <summary>
+        /// 获取长度
+        /// </summary>
+        /// <returns></returns>
+        public int GetPoolCount()
+        {
+            return Pool.Count;
+        }
+        /// <summary>
+        /// 取出
+        /// </summary>
+        /// <returns></returns>
+        public UserToken Pop()
+        {
+            return Pool.Pop();
+        }
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="userToken"></param>
+        public void Push(UserToken userToken)
+        {
+            if (userToken != null)
+            {
+                Pool.Push(userToken);
+            }
+        }
+    }
+}
