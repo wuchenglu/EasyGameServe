95e11adf3c322d00df4c3f760734a06f5bb882a4
251020148 <251020148@qq.com>
2018-07-27 23:54:23 +0800

    添加事务中心接口

---
+++ MyServer/MyServer/HandlerInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetFrame;
+using NetFrame.Auto;
+
+namespace MyServer
+{
+    interface HandlerInterface
+    {
+        /// <summary>
+        /// 客户端断开
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="error"></param>
+        void ClientClose(NetFrame.UserToken token, string error);
+        /// <summary>
+        ///信息到达
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="message"></param>
+        void MessageReceive(UserToken token, SocketModel message)
+    }
+}
